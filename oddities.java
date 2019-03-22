240ce96ee3ec3fb9eda00fdb38832c3e0bbac5e8
jocai8 <jocai@iu.edu>
2019-03-21 20:29:43 -0400

    added source

---
+++ oddities.java
@@ -0,0 +1,16 @@
+public class oddities{}import java.io.*;
+ 
+public class odditites( 
+public static void main(String[] args){
+ 
+Scanner scan = new Scanner(System.in);
+System.out.print("enter an number to see oddities: ")
+int num = scan.nextInt();
+ 
+if(num%2==0){
+System.out.print("the number is odd");
+}
+else{
+System.out.printn("the number is not odd");
+}
+
